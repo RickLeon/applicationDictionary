73c252347d3b558d06bd77da9612fe213b57e39f
Ricardo M. Leonarczyk <ricardo.leonarczyk95@gmail.com>
2018-06-07 20:14:45 -0300

    Add item under dictionaries' menu and basic support for UWP apps.

--- addon/globalPlugins/applicationDictionary.py
+++ addon/globalPlugins/applicationDictionary.py
@@ -6,6 +6,7 @@
 import api
 import globalPluginHandler
 import gui
+import wx
 import speechDictHandler
 import os
 from speechDictHandler import speechDictsPath
@@ -17,7 +18,8 @@ except:
 	SCRCAT_CONFIG = None
 
 def getAppName():
-	return api.getForegroundObject().appModule.appName
+	# From NVDA's core
+	return (gui.mainFrame.prevFocus or api.getFocusObject()).appModule.appName
 
 def getDictFilePath(appName):
 	return os.path.join(speechDictsPath, appName + ".dic")
@@ -41,8 +43,15 @@ dicts = loadEmptyDicts()
 
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
-	__currentDict = None
-	__currentAppName = None
+
+	def __init__(self):
+		self.__currentDict = None
+		self.__currentAppName = None
+		super(globalPluginHandler.GlobalPlugin, self).__init__()
+		self.dictsMenu = gui.mainFrame.sysTrayIcon.preferencesMenu.GetMenuItems()[1].GetSubMenu()
+		# Translators: The label for the menu item to open Application specific speech dictionary dialog.
+		self.appDictDialog = self.dictsMenu.Append(wx.ID_ANY, _("&Application Dictionary..."), _("A dialog where you can set application-specific dictionary by adding dictionary entries to the list"))
+		gui.mainFrame.sysTrayIcon.Bind(wx.EVT_MENU, self.script_editDict, self.appDictDialog)
 
 	def event_foreground(self, obj, nextHandler):
 		appName = getAppName()
@@ -61,14 +70,14 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 			dict = speechDictHandler.SpeechDict()
 			dict.load(dictFilePath)
 			dicts[appName] = dict
-# Translators: title of application dictionary dialog.
+		# Translators: title of application dictionary dialog.
 		title = _("Dictionary for {arg0}").format(arg0=appName)
 		gui.mainFrame._popupSettingsDialog(gui.DictionaryDialog, title, dict)
 	script_editDict.category = SCRCAT_CONFIG
-# Translators: Message presented in input help mode.
+	# Translators: Message presented in input help mode.
 	script_editDict.__doc__ = _("Shows the application-specific dictionary dialog")
 
-# Temp dictionary use taken from emoticons add-on
+	# Temp dictionary usage taken from emoticons add-on
 	def __setCurrentDict(self, dict):
 		if self.__currentDict is not None:
 			for e in self.__currentDict: speechDictHandler.dictionaries["temp"].remove(e)
